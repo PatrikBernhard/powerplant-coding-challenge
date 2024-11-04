476fd333d74358bb31c0e202e9f4a419b7b8ea97
Patrik Bernhardsson <pbernhardsson@littlebigcode.be>
2024-11-04 23:18:31 +0100

    Add model

---
+++ src/domain/model.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from enum import Enum
+
+"""
+The payload contains 3 types of data:
+ - load: The load is the amount of energy (MWh) that need to be generated during one hour.
+ - fuels: based on the cost of the fuels of each powerplant, the merit-order can be determined which is the starting point for deciding which powerplants should be switched on and how much power they will deliver.  Wind-turbine are either switched-on, and in that case generate a certain amount of energy depending on the % of wind, or can be switched off. 
+   - gas(euro/MWh): the price of gas per MWh. Thus if gas is at 6 euro/MWh and if the efficiency of the powerplant is 50% (i.e. 2 units of gas will generate one unit of electricity), the cost of generating 1 MWh is 12 euro.
+   - kerosine(euro/Mwh): the price of kerosine per MWh.
+   - co2(euro/ton): the price of emission allowances (optionally to be taken into account).
+   - wind(%): percentage of wind. Example: if there is on average 25% wind during an hour, a wind-turbine with a Pmax of 4 MW will generate 1MWh of energy.
+ - powerplants: describes the powerplants at disposal to generate the demanded load. For each powerplant is specified:
+   - name:
+   - type: gasfired, turbojet or windturbine.
+   - efficiency: the efficiency at which they convert a MWh of fuel into a MWh of electrical energy. Wind-turbines do not consume 'fuel' and thus are considered to generate power at zero price.
+   - pmax: the maximum amount of power the powerplant can generate.
+   - pmin: the minimum amount of power the powerplant generates when switched on. 
+"""
+
+class Fuel(Enum):
+    GAS = "gas"
+    KEROSINE = "kerosine"
+    CO2 = "co2"
+    WIND = "wind"
+
+
+@dataclass
+class PayLoad:
+    load: int
+    fuels: dict[Fuel, int]
+
+
+@dataclass
+class PowerPlant:
+      name: str
+      type: str
+      efficiency: float
+      pmin: int
+      pmax: int
