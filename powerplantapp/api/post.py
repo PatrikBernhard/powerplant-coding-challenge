fd19bdc1cea300b25ad0f504ca9051fc5ac1ad1f
Patrik Bernhardsson <pbernhardsson@littlebigcode.be>
2024-11-06 04:34:31 +0100

    Added healthcheck on root

--- powerplantapp/api/post.py
+++ powerplantapp/api/post.py
@@ -6,6 +6,10 @@ from powerplantapp.domain.model import PayLoad
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 @app.post("/productionplan")
 async def production_plan(payload: PayLoad):
     return compute_response(payload)
